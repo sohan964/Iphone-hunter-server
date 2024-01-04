6b57756316f53d6680b5b8e5f7557aa9d9411cb7
sohan964 <sohanurr791@gmail.com>
2024-01-04 07:31:57 +0600

    ExpressJS Server

---
+++ __SetUp.js
@@ -0,0 +1,12 @@
+/*
+    1. need to create a project folder
+    2. npm init -y inside the folder
+    3. need to create a index.js file
+    4. npm install express
+    5. npm install cors
+    6. then code on the index.js
+    7. nodemon index.js
+           --if nodemon not installed
+                npm install -g nodemon
+
+*/
